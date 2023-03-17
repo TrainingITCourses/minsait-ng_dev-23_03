d0b6e790cc5d9cfce16df4f80d0e38efcf318023
Alberto Basalo <albertobasalo@hotmail.com>
2023-03-17 13:27:50 +0100

    feat: template basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'minsait-ng_dev-23_03';
+  private authorName = 'Alberto Basalo';
+  title = 'Minsait';
+
+  getAuthor() {
+    return this.authorName;
+  }
 }
