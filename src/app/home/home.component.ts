c0d32df1f4045f3e778dbed8cde6881a30557575
Alberto Basalo <albertobasalo@hotmail.com>
2023-03-24 12:13:12 +0100

    feat: activities list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,34 @@ import { ACTIVITIES } from '../data/app.data';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  activities: Activity[] = ACTIVITIES.filter((a) => a.state === 'published');
-  constructor() {}
+  private publishedActivities: Activity[];
+  private order = 1;
+  private searchTerm = '';
+  public activities: Activity[];
 
+  constructor() {
+    this.publishedActivities = ACTIVITIES.filter(
+      (a) => a.state === 'published'
+    );
+    this.activities = this.publishedActivities;
+  }
   ngOnInit(): void {}
+
+  public onSortClick() {
+    this.order = -this.order;
+    this.setActivities();
+  }
+
+  public onSearchChange(event: any) {
+    this.searchTerm = event.target.value;
+    this.setActivities();
+  }
+
+  private setActivities() {
+    this.activities = this.publishedActivities
+      .filter((a) =>
+        a.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+      )
+      .sort((a, b) => (a.price - b.price) * this.order);
+  }
 }
