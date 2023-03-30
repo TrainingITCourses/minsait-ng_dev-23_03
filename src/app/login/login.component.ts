a1bba118cf3d8cbe23883df10af9e67aa58dfdcb
Alberto Basalo <albertobasalo@hotmail.com>
2023-03-30 17:13:30 +0200

    feat: form login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  credentials: Credentials = {
+    email: '',
+    password: '',
+  };
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  onLoginClick() {
+    console.log('Login Click', this.form.value);
+  }
+
+  showError(controlName: string): boolean {
+    const control = this.form.controls[controlName];
+    if (!control) return false;
+    return control.invalid && (control.touched || control.dirty);
+  }
 }
+
+type Credentials = {
+  email: string;
+  password: string;
+};
