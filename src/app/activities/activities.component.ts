6eb53fed91bb650c1d95d0db9169814ab96566e0
Alberto Basalo <albertobasalo@hotmail.com>
2023-03-31 13:05:24 +0200

    feat: pipe catch error

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -9,18 +9,24 @@ import { Activity } from '../data/activity.type';
   styleUrls: ['./activities.component.css'],
 })
 export class ActivitiesComponent implements OnInit {
-  activities: Activity[] = []; //this.activitiesService.getAllActivities();
+  //activities: Activity[] = []; //this.activitiesService.getAllActivities();
   activities$!: Observable<Activity[]>;
 
   constructor(private activitiesService: ActivitiesService) {}
 
   ngOnInit(): void {
-    this.activitiesService.getAllActivities$().subscribe(
-      (activities) => {
-        this.activities = activities;
-      },
-      (error) => (this.activities = [])
-    );
+    // this.activitiesService.getAllActivities$().subscribe(
+    //   (activities) => {
+    //     this.activities = activities;
+    //   },
+    //   (error) => (this.activities = []),
+    //   () => console.log('End')
+    // );
+    // this.activitiesService.getAllActivities$().subscribe({
+    //   next: (activities) => (this.activities = activities),
+    //   error: (error) => (this.activities = []),
+    //   complete: () => console.log('End'),
+    // });
 
     this.activities$ = this.activitiesService
       .getAllActivities$()
